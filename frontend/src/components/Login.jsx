98bcb57ba082da2d422152a4d2a62fb56d283193
Maham Taj <maham.taj@learnit.fi>
2024-07-04 17:09:20 +0300

    Changed local host

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -27,7 +27,7 @@ const Login = ({ setIsAuthenticated }) => {
     try {
       const token = localStorage.getItem('token');
       if (token) {
-        const response = await axios.get('http://localhost:3000/api/auth/verify', {
+        const response = await axios.get('http://localhost:5000/api/auth/verify', {
           //{ Authorization: token }
           headers: { Authorization: `Bearer ${token}` }
         });
